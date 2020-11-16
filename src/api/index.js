06b72b1a1094c8bc6755cf49d9cff41693b2f54e
liuyuan <1316425655@qq.com>
2020-11-16 17:23:50 +0800

    day01

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+export {default as login} from './acl/login' // {default: {}}
+export {default as user} from './acl/user'
+export {default as role} from './acl/role'
+export {default as permission} from './acl/permission'
+export {default as category} from './category'
+export {default as clientUser} from './clientUser'
+export {default as order} from './order'
+export {default as trademark} from './product/trademark'
+
+
+//export {default as category} from './category'本质
+// import {default as category} from './category'
+// export category  {全是api接口请求函数}
+
+
+// //export {default as order} from './order' 本质
+// import {default as order} from './order'
+// export order {全是api接口请求函数}
+
+
+
+
+// {
+//   category，
+//   order，
+// }
+
